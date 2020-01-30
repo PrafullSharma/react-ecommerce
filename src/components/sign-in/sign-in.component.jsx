2b2a67b3d77e6a474c46077d338a41bd48ba402e
Prafull Sharma <prafull.sharma@ekino.com>
2020-01-30 13:18:34 +0530

    80. Adding a Project to Firebase - 82. Google Sign In Authentication

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import {signInWithGoogle} from '../../firebase/firebase.utils'
 
 import './sign-in.styles.scss';
 
@@ -50,7 +51,8 @@ class SignIn extends React.Component {
             label='password'
             required
           />
-          <CustomButton type='submit'> Sign in </CustomButton>
+          <CustomButton type='submit'> Sign in </CustomButton>{'  '}
+          <CustomButton onClick={signInWithGoogle}> Sign in with Google </CustomButton>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKDYadD3m3EsXGbhNcwhjWkqkwVYPuz38",
+  authDomain: "small-ecomerce-db.firebaseapp.com",
+  databaseURL: "https://small-ecomerce-db.firebaseio.com",
+  projectId: "small-ecomerce-db",
+  storageBucket: "small-ecomerce-db.appspot.com",
+  messagingSenderId: "905153141099",
+  appId: "1:905153141099:web:4e5f9b975d6e0140df142e"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({prompt: 'select_account'})
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
+
+export default firebase
