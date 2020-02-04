d9c74bdce7681482a1e1cd0d0a62d1b2f6a4d04a
Prafull Sharma <prafull.sharma@ekino.com>
2020-02-04 15:38:49 +0530

    162. Bringing Shop Data To Our App - 163. Adding Shop Data To Redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,17 +15,17 @@ const config = {
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if(!userAuth) return
+  if (!userAuth) return
 
   const userRef = firestore.doc(`users/${userAuth.uid}`)
   const snapShot = await userRef.get()
-  
-  if(!snapShot.exists) {
-    const {displayName, email} = userAuth
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth
     const createdAt = new Date()
 
     try {
-      await userRef.set({displayName, email, createdAt, ...additionalData})
+      await userRef.set({ displayName, email, createdAt, ...additionalData })
     } catch (error) {
       console.log('Error: error while creating user', error.message)
     }
@@ -36,7 +36,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
   const collectionRef = firestore.collection(collectionKey)
-  
+
   const batch = firestore.batch()
   objectsToAdd.forEach(obj => {
     const newDocRef = collectionRef.doc()
@@ -44,6 +44,24 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   });
 
   return await batch.commit()
+}
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data()
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  })
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection
+    return accumulator
+  }, {})
 }
 
 export const auth = firebase.auth();
